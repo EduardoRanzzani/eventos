55260f8f3d4c49252fc7a7738919aa45913850d8
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2024-11-24 16:36:30 -0400

    Dia 03: Vídeo 59 - Integração com o prisma

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EventosModule } from './eventos/eventos.module';
+import { DbModule } from './db/db.module';
 
 @Module({
   controllers: [AppController],
-  imports: [EventosModule],
+  imports: [EventosModule, DbModule],
 })
 export class AppModule {}
